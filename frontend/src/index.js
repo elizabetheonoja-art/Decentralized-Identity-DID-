4ba1c76b87dc09cc5ffca38453d9a28c6b499a26
VictorEzenma <ezenmavictor2002@gmail.com>
2026-03-29 03:14:42 +0100

    Implemented offline functionality using service workers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,3 +9,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("SW registered:", reg))
+      .catch((err) => console.log("SW failed:", err));
+  });
+}
